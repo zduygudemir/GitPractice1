0311e36e9bc318e557434eef81cf87c2839b1c88
Zahir-Demir <melekceylin@hotmail.com>
2021-04-24 16:57:24 -0400

    Added second line

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Adding GitHub Now");
         System.out.println("YAY STUFFS ARE ON GITHUB !!!");
+        //adding final wrap up code
     }
 }
